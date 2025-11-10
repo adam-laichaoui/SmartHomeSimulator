ed2a82dabe78448f267f4698b8f66f21d6727754
Francesco Marchetti <francymarchtti04@gmail.com>
2025-11-10 16:31:23 +0100

    corretto qualche errore

--- HomeSimulator/src/main/Exporter.java
+++ HomeSimulator/src/main/Exporter.java
@@ -5,22 +5,35 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Exporter {
-    public static void esporta(Map<String, DatoSensore> dati) {
+    public static void esportaStorico(Map<String, List<DatoSensore>> storico) {
         try {
-            String desktopPath = System.getProperty("user.home") + File.separator + "Desktop" + File.separator + "CasaIoT_Dati";
+            String desktopPath = System.getProperty("user.home") + File.separator +
+                    "Desktop" + File.separator + "CasaIoT_Dati";
             File folder = new File(desktopPath);
             if (!folder.exists()) folder.mkdirs();
 
             String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-            File file = new File(folder, "sensori_" + timestamp + ".txt");
+            File file = new File(folder, "storico_sensori_" + timestamp + ".txt");
 
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-                for (Map.Entry<String, DatoSensore> entry : dati.entrySet()) {
-                    DatoSensore d = entry.getValue();
-                    writer.write(entry.getKey() + ": " + d.getValore() + " " );
+                writer.write("=== Storico dati sensori ===");
+                writer.newLine();
+                writer.write("Generato: " + new Date());
+                writer.newLine();
+                writer.newLine();
+
+                for (Map.Entry<String, List<DatoSensore>> entry : storico.entrySet()) {
+                    writer.write("Sensore: " + entry.getKey());
+                    writer.newLine();
+                    for (DatoSensore d : entry.getValue()) {
+                        writer.write("  " + d.getOraFormattata() + "  →  " + d.getValore() + " " + d.getUnitaMisura());
+                        writer.newLine();
+                    }
                     writer.newLine();
                 }
             }
+
+            System.out.println("File esportato in: " + file.getAbsolutePath());
         } catch (IOException e) {
             e.printStackTrace();
         }
